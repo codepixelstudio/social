b53fd1bdb15cc59c56d7fd0fe7f2e162f6bd13db
codepixelstudio <codepixel@codepixelstudio.com>
2021-01-25 11:57:06 -0700

    install Google Analytics tracking code

--- header.php
+++ header.php
@@ -15,6 +15,18 @@
 
 		<?php wp_head(); ?>
 
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo $analytics_ID; ?>"></script>
+        <script>
+
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push( arguments );}
+            gtag( 'js', new Date() );
+
+            gtag( 'config', 'UA-11394918-16' );
+
+        </script>
+
 	</head>
 
 	<body <?php body_class(); ?> data-viewport="desktop">
