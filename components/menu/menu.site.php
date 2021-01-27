2abeca5d1f4f8f370a0bea258bcf826d22a8deaf
codepixelstudio <codepixel@codepixelstudio.com>
2021-01-27 09:19:49 -0700

    configure MailChimp signup form for production server

--- components/menu/menu.site.php
+++ components/menu/menu.site.php
@@ -26,7 +26,7 @@
     <div class="newsletter_signup">
 
         <!-- form -->
-        <form id="newsletter_signup_form">
+        <form action="https://colostate.us16.list-manage.com/subscribe/post?u=6b85dbdc3e85eef3354908bed&amp;id=a817b78433" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 
             <!-- field container -->
             <div class="form_fields">
@@ -34,16 +34,34 @@
                 <!-- input wrap -->
                 <div class="input_wrap">
 
-                    <label for="email">e-mail address</label>
-                    <input id="email" class="text_input" type="email" name="email" value="" placeholder="subscribe to our newsletter">
+                    <label for="EMAIL">e-mail address</label>
+                    <input id="mce-EMAIL" class="text_input" type="email" name="EMAIL" value="" placeholder="subscribe to our newsletter">
 
                 </div>
                 <!-- END input wrap -->
 
-                <input type="submit" name="" value="subscribe">
+                <input type="submit" value="subscribe" name="subscribe" id="mc-embedded-subscribe" class="required email">
 
             </div>
             <!-- END field container -->
+
+            <!-- responses -->
+            <div id="mce-responses" class="clear">
+
+                <div class="response" id="mce-error-response" style="display:none"></div>
+                <div class="response" id="mce-success-response" style="display:none"></div>
+
+            </div>
+            <!-- END  responses -->
+
+            <!-- honeypot -->
+            <div style="position: absolute; left: -5000px; height: 0;" aria-hidden="true">
+
+                <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                <input type="text" name="b_6b85dbdc3e85eef3354908bed_a817b78433" tabindex="-1" value="">
+
+            </div>
+            <!-- END honeypot -->
 
         </form>
         <!-- END form -->
