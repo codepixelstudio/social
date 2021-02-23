e4c672bb78d4aa1ca1cdd9f93fe08ace1bb430e9
codepixelstudio <codepixel@codepixelstudio.com>
2021-02-23 10:08:29 -0700

    remove post formats

--- library/theme-support.php
+++ library/theme-support.php
@@ -190,24 +190,7 @@
 			// post thumbnail
 			add_theme_support( 'post-thumbnails' );
 
-			// post formats
-			add_theme_support(
-
-				'post-formats', array(
-
-					// 'aside',
-					// 'gallery',
-					// 'link',
-					// 'image',
-					// 'quote',
-					// 'status',
-					// 'video',
-					// 'audio',
-					// 'chat'
-
-				)
-
-			);
+			// post formats -> REMOVED
 
 		}
 
